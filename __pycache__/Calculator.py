44eb47a744152162f32124782481f4094606c435
Nabeel-24 <nabeelahmed.e.n@gmail.com>
2020-08-18 10:55:13 +0000

    Caculator

---
+++ __pycache__/Calculator.py
@@ -0,0 +1,27 @@
+def add(a,b):
+    a+b
+    return(a+b)
+
+def sub(a,b):
+    a-b
+    return(a-b)
+
+def multiply(a,b):
+    a*b
+    return(a*b)
+
+def divide(a,b):
+    a/b
+    return(a/b)
+
+def square(n):
+    n**2
+    return(n**2)
+
+def cube(n):
+    n**3
+    return(n**3)
+
+def percentage(n,a):
+    n/a*100
+    return(n/a*100)
